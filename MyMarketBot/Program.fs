c0ee3710d5c6ec7f1a769d9ee765465bbd783323
Vorotynsky Maxim <vorotynsky.maxim@gmail.com>
2021-01-30 03:10:41 +0300

    feat(moex): send indexes to user
    Load IMOEX, RTSI and RGBI for today and yesterday and send it to user.
    
    todo: format message

--- MyMarketBot/Program.fs
+++ MyMarketBot/Program.fs
@@ -2,13 +2,25 @@
 
 open System
 open MyMarketBot.Telegram
+open MyMarketBot.Moex
+
+let sendIndexData chatId bot index = async {
+    let! data = loadData index DateTime.Today
+    do! send chatId (sprintf "%s: %A" index data) bot |> Async.Ignore
+}
 
 [<EntryPoint>]
 let main _ =
     let token = Environment.GetEnvironmentVariable "TELEGRAM_BOT_TOKEN"
-    
+    let chatId = Environment.GetEnvironmentVariable "TELEGRAM_SUBSCRIBER_ID" |> Int64.Parse
+   
     use bot = run token
     
-    Console.WriteLine "Press key to stop"
-    Console.ReadKey() |> ignore
+    let task = 
+        [| "IMOEX"; "RTSI"; "RGBITR" |]
+        |> Array.map (sendIndexData chatId bot)
+        |> Async.Parallel
+        |> Async.StartAsTask
+        
+    task.Wait()
     0
