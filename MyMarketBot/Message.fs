9664b1e21aad7ff9a054fd94e57ab6b275d53b89
Vorotynsky Maxim <vorotynsky.maxim@gmail.com>
2021-01-31 21:30:50 +0300

    refactor: extract message generation and indexes loading
     - Message generation moved to `Message` module
     - Index loading moved from `Moex` to `Modex.Index` and `Moex.Common`

---
+++ MyMarketBot/Message.fs
@@ -0,0 +1,32 @@
+﻿module MyMarketBot.Message
+
+open System
+open System.Text
+open MyMarketBot.Moex.Common
+open MyMarketBot.Moex.Index
+
+let toString formatter = function
+        | NoDataForADay -> "N/D"
+        | Today today -> sprintf "%s" (formatter today)
+        | TwoDays (today, yesterday) ->
+            sprintf "%s (%+.2f%%)" (formatter today) (float((today - yesterday) / yesterday * 100M))
+          
+
+let prepareMessage (indexes: (string * MoexDayData)[]) =
+    let indexes = indexes |> dict
+    let message index = sprintf "%s: %s" index (toString (sprintf "%.2f") indexes.[index])
+    
+    let builder = StringBuilder()
+    let sbprintf = Printf.bprintf builder
+    
+    
+    sbprintf "%s итоги дня:\n\n" (DateTime.Today.ToShortDateString())
+    
+    builder.AppendJoin("\n", Array.map message moexIndexes) |> ignore
+    
+    sbprintf "\n\n Spread index: %s"
+        (toString
+             (sprintf "%.2f%%" << rangeSpreadIndex)
+             (spreadIndex indexes.["IMOEX"] indexes.["RGBITR"]))
+
+    builder.ToString()
---
+++ MyMarketBot/Moex/Common.fs
@@ -0,0 +1,29 @@
+﻿module MyMarketBot.Moex.Common
+
+open System
+
+type MoexDayData =
+    | NoDataForADay
+    | Today of decimal
+    | TwoDays of today: decimal * yesterday: decimal
+
+let (|HasToday|_|) = function
+    | NoDataForADay -> None
+    | Today today -> Some today
+    | TwoDays (today, _) -> Some today
+
+let makeUrlBuilder (url: string) (_index: string) _start _end index startDate endDate =
+    let formatDate (date: DateTime) = date.ToString "yy-MM-dd"
+    url.Replace(_index, index)
+       .Replace(_start, formatDate startDate)
+       .Replace(_end, formatDate endDate)
+       
+let yesterday (now: DateTime) =
+    if now.DayOfWeek = DayOfWeek.Monday then 3.0 else 1.0
+    |> TimeSpan.FromDays
+    |> now.Subtract
+    
+let makeData = function
+    | [| today |] -> Today today
+    | [| today; yesterday |] -> TwoDays (today, yesterday)
+    | _ -> NoDataForADay
